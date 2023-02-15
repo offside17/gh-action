d6f7e39048f25243e7e5d4b225c0689bb8d8ef39
offside17 <neofierabras1@gmail.com>
2023-02-15 17:11:38 +0100

    p

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import { readFile, writeFile } from 'node:fs/promises'
+import core from '@actions/core'
 
 async function run() {
+  const data_path = core.getInput('data_path')
+
   console.log('Action lanzada 1')
-  console.log(process.env.TEST_VAR)
+  console.log(data_path)
   console.log('Action lanzada 2')
   // const file = await readFile('./src/file.md', 'utf-8')
   // console.log(file)
