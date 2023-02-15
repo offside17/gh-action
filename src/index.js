5489ac4d6507586539b9c51c611b903955aa544b
offside17 <neofierabras1@gmail.com>
2023-02-15 17:01:28 +0100

    f

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import { readFile, writeFile } from 'node:fs/promises'
 
 async function run() {
-  console.log('Action lanzada')
+  console.log('Action lanzada 1')
   console.log(env.TEST_VAR)
+  console.log('Action lanzada 2')
   // const file = await readFile('./src/file.md', 'utf-8')
   // console.log(file)
 }
