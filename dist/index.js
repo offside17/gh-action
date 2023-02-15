a3a468bc1b4df2efa66a5872897dbf9affd17b3d
offside17 <neofierabras1@gmail.com>
2023-02-15 16:42:10 +0100

    add dist

---
+++ dist/index.js
@@ -0,0 +1,20 @@
+import { createRequire as __WEBPACK_EXTERNAL_createRequire } from "module";
+/******/ /* webpack/runtime/compat */
+/******/ 
+/******/ if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = new URL('.', import.meta.url).pathname.slice(import.meta.url.match(/^file:\/\/\/\w:/) ? 1 : 0, -1) + "/";
+/******/ 
+/************************************************************************/
+var __webpack_exports__ = {};
+
+;// CONCATENATED MODULE: external "node:fs/promises"
+const promises_namespaceObject = __WEBPACK_EXTERNAL_createRequire(import.meta.url)("node:fs/promises");
+;// CONCATENATED MODULE: ./src/index.js
+
+
+async function run() {
+  const file = await (0,promises_namespaceObject.readFile)('./src/file.md', 'utf-8')
+  console.log(file)
+}
+
+run()
+
