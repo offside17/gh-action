fd884017b08699e323850b5498e894a28e61f64e
offside17 <neofierabras1@gmail.com>
2023-02-15 17:03:38 +0100

    f

--- dist/index.js
+++ dist/index.js
@@ -12,7 +12,9 @@ const promises_namespaceObject = __WEBPACK_EXTERNAL_createRequire(import.meta.ur
 
 
 async function run() {
-  console.log('Action lanzada')
+  console.log('Action lanzada 1')
+  console.log(env.TEST_VAR)
+  console.log('Action lanzada 2')
   // const file = await readFile('./src/file.md', 'utf-8')
   // console.log(file)
 }
